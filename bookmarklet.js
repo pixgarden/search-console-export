381bd03fed564a44f46d28c2d1752412a5c09baa
François <pixgarden@gmail.com>
2016-07-07 16:09:54 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,1 @@
-var output2 = document.querySelectorAll('#wmt-tsq > div > div.content-region > div.overtime-chart-container > div.overtime-gviz-chart > div > div:nth-child(1) > div > div > table');
-var d = new Date();
-var curr_day = d.getDate();
-var curr_month = d.getMonth() + 1; //Months are zero based
-var curr_year = d.getFullYear();
-var filename = 'GWT-' + document.title.split('-')[2] + '_' + curr_year + '-' + curr_month + '-' + curr_day;
-var pom = document.createElement('a');
-pom.setAttribute('href', 'data:text/csv;charset=utf-8,' + output2[0].innerHTML.replace(/<\/td\>\<td\>/g, ";").replace(/\<\/td\>\<\/tr\>\<tr\>\<td\>/g, `*`));
-pom.setAttribute('download', filename + '.csv');
-pom.click();
+javascript:output2 = document.querySelectorAll('#wmt-tsq > div > div.content-region > div.overtime-chart-container > div.overtime-gviz-chart > div > div:nth-child(1) > div > div > table');d = new Date();curr_day = d.getDate();curr_month = d.getMonth() + 1;curr_year = d.getFullYear();filename = 'GWT-' + document.title.split('-')[2] + '_' + curr_year + '-' + curr_month + '-' + curr_day;pom = document.createElement('a');pom.setAttribute('href', 'data:text/csv;charset=utf-8,' + output2[0].innerHTML.replace(/<\/td\>\<td\>/g, ";").replace(/\<\/td\>\<\/tr\>\<tr\>\<td\>/g, `*`));pom.setAttribute('download', filename + '.csv');pom.click();
