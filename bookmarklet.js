3a6fc583e2affbae60ba1ccbcfbc314ff8a4abce
François <pixgarden@gmail.com>
2016-07-07 15:44:59 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,7 @@ var d = new Date();
 var curr_day = d.getDate();
 var curr_month = d.getMonth() + 1; //Months are zero based
 var curr_year = d.getFullYear();
-var filename = 'GWT-' + document.URL.slice(-14, -11) + '_' + curr_year + '-' + curr_month + '-' + curr_day;
+var filename = 'GWT-' + document.title.split('-')[2] + '_' + curr_year + '-' + curr_month + '-' + curr_day;
 var pom = document.createElement('a');
 pom.setAttribute('href', 'data:text/csv;charset=utf-8,' + output2[0].innerHTML.replace(/<\/td\>\<td\>/g, ";").replace(/\<\/td\>\<\/tr\>\<tr\>\<td\>/g, `*`));
 pom.setAttribute('download', filename + '.csv');
