fd169f04489a7cec08be3b791802177f360f516f
François <pixgarden@gmail.com>
2016-07-07 15:43:04 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,6 +5,6 @@ var curr_month = d.getMonth() + 1; //Months are zero based
 var curr_year = d.getFullYear();
 var filename = 'GWT-' + document.URL.slice(-14, -11) + '_' + curr_year + '-' + curr_month + '-' + curr_day;
 var pom = document.createElement('a');
-pom.setAttribute('href', 'data:text/csv;charset=utf-8,' + output2[0].innerHTML.replace(/<\/td\>\<td\>/g, ";").replace(/\<\/td\>\<\/tr\>\<tr\>\<td\>/g, "****"));
+pom.setAttribute('href', 'data:text/csv;charset=utf-8,' + output2[0].innerHTML.replace(/<\/td\>\<td\>/g, ";").replace(/\<\/td\>\<\/tr\>\<tr\>\<td\>/g, `*`));
 pom.setAttribute('download', filename + '.csv');
 pom.click();
